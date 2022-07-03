6ee94976caf582e46e49d0125e15b63a8d5c22a3
Natalia S. Verdura <natalia.solange.verdura@gmail.com>
2022-07-02 22:07:04 -0300

    Se arregla la ruta de gaurdado de imagenes

--- server/memes/memes.routes.js
+++ server/memes/memes.routes.js
@@ -4,7 +4,7 @@ const multer  = require('multer')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './public/memes/uploads/')
+        cb(null, '../../build/memes/uploads/')
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
