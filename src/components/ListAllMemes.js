aff799986d58829dc74e729a691277a01d1b31c4
Natalia S. Verdura <natalia.solange.verdura@gmail.com>
2022-07-04 00:15:06 -0300

    Se agrega boton de descarga de los memes

--- src/components/ListAllMemes.js
+++ src/components/ListAllMemes.js
@@ -3,10 +3,20 @@ import Card from 'react-bootstrap/Card';
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import CardGroup from "react-bootstrap/CardGroup";
+import fileDownload from "js-file-download";
+import axios from "axios";
 
 
 function ListAllMemes(props) {
     const memes = props.memes
+    const handleDownload = (url, filename) => {
+        axios.get(url, {
+                responseType: "blob"
+            })
+            .then((res) => fileDownload(res.data, filename));
+    };
+
+
     return (
         <>
             {memes && <CardGroup className="m-5 d-flex justify-content-center">
@@ -20,7 +30,7 @@ function ListAllMemes(props) {
                                         <Card.Subtitle className="mb-2 text-muted">Temporada {meme.season} -
                                             Episodio {meme.episode}</Card.Subtitle>
                                         <Card.Text> {meme.description}</Card.Text>
-                                        <Button href={meme.meme_img_url} variant="light" download>
+                                            <Button variant="light" onClick={() => {handleDownload(meme.meme_img_url, meme.season+'_'+meme.episode+"_"+meme.title)}}>
                                             <img src="../download.svg" alt=""/>
                                         </Button>
                                         {props.admin && <Button variant="light"> Editar </Button>}
