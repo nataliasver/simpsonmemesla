e79558673187ead870389bbf1dd02a8bfb405412
Natalia S. Verdura <natalia.solange.verdura@gmail.com>
2022-07-03 23:46:19 -0300

    Se reemplaza form viejo por form de bootstrap

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -3,11 +3,16 @@ import axios from "axios";
 import Promise from "bluebird"
 import {useState, useEffect} from "react";
 import _ from "lodash"
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Image from 'react-bootstrap/Image'
 
 function UploadForm() {
     const [memes, setMemes] = useState(null);
     const [file, setFile]= useState(null);
-    const [preview, setPreview] = useState(null);
+    const [preview, setPreview] = useState("no_image.jpg");
 
     useEffect(() => {
         if (file == null) return;
@@ -58,55 +63,57 @@ function UploadForm() {
 
     return(
         <>
-            <p>Carga de nuevo meme</p>
-            <form style={{ display: "flex", flexDirection: "column", alignItems: "center"}} onSubmit={handleSubmit} >
-                <label htmlFor="title">Titulo</label>
-                <input
-                    id="title"
-                    name="title"
-                    type="text"
-                    value={values.title}
-                    onChange={handleChange}
-                />
-                <label htmlFor="season">Season</label>
-                <input
-                    id="season"
-                    name="season"
-                    type="number"
-                    value={values.season}
-                    onChange={handleChange}
-                />
-                <label htmlFor="episode">Episodio</label>
-                <input
-                    id="episode"
-                    name="episode"
-                    type="number"
-                    value={values.episode}
-                    onChange={handleChange}
-                />
-                <label htmlFor="description">Descripción</label>
-                <textarea
-                    id="description"
-                    name="description"
-                    type="text"
-                    value={values.description}
-                    onChange={handleChange}
-                />
-                <label htmlFor="characters">Personajes</label>
-                <input
-                    id="characters"
-                    name="characters"
-                    type="text"
-                    value={values.characters}
-                    onChange={handleChange}
-                />
-                <label htmlFor="meme">Meme</label>
-                <input type="file" id="uploaded_file" name="uploaded_file" onChange={handleFileChange}/>
-                {
-                    preview && (<img style={{width: 300, height: 200}} src={preview} />)
-                }
-                <button type="submit">Cargar nuevo meme</button>
-            </form>
+            <div className="d-flex justify-content-center m-3">
+            <Form className="w-75" onSubmit={handleSubmit}>
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="formGridTitle">
+                        <Form.Label>Titulo</Form.Label>
+                        <Form.Control
+                            type="text" name="title" value={values.title} onChange={handleChange}
+                            placeholder="Ingrese titulo del meme"/>
+                    </Form.Group>
+                </Row>
+
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="formGridSeason">
+                        <Form.Label>Temporada</Form.Label>
+                        <Form.Control type="number" name="season" value={values.season} onChange={handleChange} />
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="formGridEpisode">
+                        <Form.Label>Episodio</Form.Label>
+                        <Form.Control type="number" name="episode" value={values.episode} onChange={handleChange} />
+                    </Form.Group>
+                </Row>
+                <Row className="mb-3">
+                <Form.Group as={Col} className="mb-3" controlId="formGridEpisode">
+                    <Form.Label>Descripcion</Form.Label>
+                    <Form.Control as="textarea" type="text" name="description" value={values.description} onChange={handleChange}
+                                  placeholder="Ingrese descripcion del meme"/>
+                </Form.Group>
+
+                <Form.Group as={Col} className="mb-3" controlId="formGridEpisode">
+                    <Form.Label>Personajes</Form.Label>
+                    <Form.Control as="textarea" type="text" name="characters" value={values.characters} onChange={handleChange}
+                    placeholder="Ingrese nombres de personajes separados por comas"/>
+                </Form.Group>
+
+            </Row>
+
+                <Row className="mb-3">
+                <Form.Group controlId="formFileMeme" className="mb-3 w-50">
+                    <Form.Label>Meme</Form.Label>
+                    <Form.Control type="file" size="sm" id="uploaded_file" name="uploaded_file" onChange={handleFileChange} />
+                </Form.Group>
+                    <Form.Group className="w-50" controlId="fromMemePreview">
+                    <Image src={preview} style={{width: 400, height: 250 }} alt="preview del meme a subir"/>
+                    </Form.Group>
+                    </Row>
+                <Button variant="secondary" type="submit">
+                    Upload meme
+                </Button>
+            </Form>
+            </div>
 
         </>
     );
